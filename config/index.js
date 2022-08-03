22c37b04edd319e20f2f8d5ca929c69e734bed3e
shino <1471386835@qq.com>
2022-08-03 17:27:37 +0800

    上线

--- config/index.js
+++ config/index.js
@@ -3,5 +3,7 @@
  */
 
 module.exports = {
-    URL: 'mongodb://127.0.0.1:27017/company-manager'
+    // URL: 'mongodb://127.0.0.1:27017/company-manager'
+    URL: 'mongodb://mongo:M8Qh2elKWySyd3sfCd4b@containers-us-west-32.railway.app:7062/company-manager?authSource=admin'
 }
+// "authenticationDatabase=admin"
